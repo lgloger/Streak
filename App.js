e36970beafdce31c1ad5bae4e86102a3de40595d
Luca <luca.kloger@gmail.com>
2025-02-11 21:45:14 +0100

    Test

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback } from "react";
 import { StyleSheet, Text, TouchableOpacity, Image, View } from "react-native";
 import * as NavigationBar from "expo-navigation-bar";
-import * as Font from "expo-font";
+import { useFonts } from "expo-font";
 import AppNavigator from "./AppNavigator";
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  useEffect(() => {
-    async function loadFonts() {
-      await Font.loadAsync({
-        "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
-        "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
-        "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
-        "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
-      });
-      setFontsLoaded(true);
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
     }
+  }, [fontsLoaded]);
 
-    loadFonts();
+  if (!fontsLoaded) {
+    return null;
+  }
 
-    NavigationBar.setBackgroundColorAsync("#E8E8E8");
-  }, []);
+  NavigationBar.setBackgroundColorAsync("#E8E8E8");
 
   if (!fontsLoaded) {
     return <Text>Loading</Text>;
